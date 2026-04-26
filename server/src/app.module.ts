487d3a54b9979ef8fa211227c3287503da142154
Kr1st10 <football76@inbox.ru>
2026-04-26 12:59:34 +0400

    Подключаем PostgreSQL и TypeORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,22 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { getDatabaseConfig } from "./database/database.config";
 
-@Module({})
+@Module({
+  imports: [
+    // 
+    // читаем env. 
+    // forRoot используется для первичной настройки модуля конфигурации, 
+    // а isGlobal: true делает ConfigService доступным во всем приложении 
+    // без повторного импорта ConfigModule в каждом модуле
+    ConfigModule.forRoot({ 
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: getDatabaseConfig
+    })
+  ]
+})
 export class AppModule {}
