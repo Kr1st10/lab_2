c1d49dac7fa1f652c2aab7bd9789ad2414e980e4
Kr1st10 <football76@inbox.ru>
2026-04-26 21:13:30 +0400

    Проверка Входных Данных

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
@@ -7,6 +8,13 @@ async function bootstrap() {
   app.enableCors({
     origin: "http://localhost:5173"
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
 
   await app.listen(3001);
 }
--- server/src/storage/dto/get-items-query.dto.ts
+++ server/src/storage/dto/get-items-query.dto.ts
@@ -1,6 +1,15 @@
+import { IsIn, IsOptional, IsString } from "class-validator";
+
 export type FileFilter = "all" | "image" | "video" | "music" | "document" | "other";
 
+const fileFilters: FileFilter[] = ["all", "image", "video", "music", "document", "other"];
+
 export class GetItemsQueryDto {
+  @IsOptional()
+  @IsString()
   search?: string;
+
+  @IsOptional()
+  @IsIn(fileFilters)
   type?: FileFilter;
 }
